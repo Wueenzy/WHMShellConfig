0003991ef3c133dd3c42bb36e4c3345d331b7a0c
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-09-19 15:59:00 +0300

    Add some lua_ls lsp config
    Enable codelens and configure hints

--- lua/whoami/plugins/lsp/lspconfig.lua
+++ lua/whoami/plugins/lsp/lspconfig.lua
@@ -161,9 +161,19 @@ return {
 								maxPreload = 100000,
 								preloadFileSize = 10000,
 							},
-
+							codeLens = {
+								enable = true,
+							},
 							completion = {
 								callSnippet = "Replace",
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
 							},
 						},
 					},
