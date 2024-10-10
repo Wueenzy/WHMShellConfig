8e3c24a45de267757c9eddcdd1c633d18e7582be
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-10-10 21:02:10 +0300

    Initialize installing script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Link files
+ln "$HOME/.whm_shell/zshrc" "$HOME/.zshrc"
+ln "$HOME/.whm_shell/myshell" "$HOME/.myshell"
+ln "$HOME/.whm_shell/p10k.zsh" "$HOME/.p10k.zsh"
+ln "$HOME/.whm_shell/vimrc" "$HOME/.vimrc"
+
