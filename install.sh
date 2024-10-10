846c2a211a953685e272e35260c3e61faff266b0
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-10-10 21:22:41 +0300

    Change install.sh file
    Adding ensure functions and install repository automaticly

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
+WHMCONFIG="$HOME/.whm_shell";
+
+ensure ()
+{
+  local program="$(command -v $1)"; shift
+  if ! [ -x "$program" ]; then
+    echo "Error: $(basename $program) is not installed." >&2
+    return 1
+  fi
+  $program $@
+}
+
+roq () # run or quit
+{
+  if ! ensure $@; then
+    exit 1;
+  fi
+}
+
+if ![[ -d "$WHMCONFIG" ]]; then
+  roq git clone "https://github.com/VKWHM/WHMShellConfig.git" "$WHMCONFIG"
+fi
 
 # Link files
-ln "$HOME/.whm_shell/zshrc" "$HOME/.zshrc"
-ln "$HOME/.whm_shell/myshell" "$HOME/.myshell"
-ln "$HOME/.whm_shell/p10k.zsh" "$HOME/.p10k.zsh"
-ln "$HOME/.whm_shell/vimrc" "$HOME/.vimrc"
-
+ln "$WHMCONFIG/zshrc" "$HOME/.zshrc"
+ln "$WHMCONFIG/myshell" "$HOME/.myshell"
+ln "$WHMCONFIG/p10k.zsh" "$HOME/.p10k.zsh"
+ln "$WHMCONFIG/vimrc" "$HOME/.vimrc"
