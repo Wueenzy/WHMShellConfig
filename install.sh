4708a046f6a49d55656963cb95340a956a5e57fd
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-10-11 05:39:01 +0000

    Adding dependency check before link configuration files

--- install.sh
+++ install.sh
@@ -1,29 +1,69 @@
 #!/bin/bash
 WHMCONFIG="$HOME/.whm_shell";
 
+# functions
+
+lnif ()
+{
+  local dst="${@: -1}";
+  if ! [[ -e "$dst" ]]; then
+    ln $@;
+  fi
+}
+
 ensure ()
 {
-  local program="$(command -v $1)"; shift
+  local program="$(command -v $1)";
   if ! [ -x "$program" ]; then
-    echo "Error: $(basename $program) is not installed." >&2
-    return 1
+    echo "[-] Error: $1 is not installed." >&2;
+    return 1;
   fi
-  $program $@
+  return 0;
 }
 
-roq () # run or quit
+ensure-all ()
 {
-  if ! ensure $@; then
+  for program in "$@"
+  do
+    if ! ensure $program; then
+      return 1;
+    fi
+  done
+}
+
+safe-run () # run or quit
+{
+  local program=$1; shift;
+  if ! ensure $program;  then
     exit 1;
   fi
+  $program $@;
 }
 
-if ![[ -d "$WHMCONFIG" ]]; then
-  roq git clone "https://github.com/VKWHM/WHMShellConfig.git" "$WHMCONFIG"
+if ! [[ -d "$WHMCONFIG" ]]; then
+  safe-run git clone "https://github.com/VKWHM/WHMShellConfig.git" "$WHMCONFIG"
 fi
 
 # Link files
-ln "$WHMCONFIG/zshrc" "$HOME/.zshrc"
-ln "$WHMCONFIG/myshell" "$HOME/.myshell"
-ln "$WHMCONFIG/p10k.zsh" "$HOME/.p10k.zsh"
-ln "$WHMCONFIG/vimrc" "$HOME/.vimrc"
+if ensure-all zsh fd eza zoxide fzf bat; then
+  lnif -s "$WHMCONFIG/myshell" "$HOME/.myshell"
+  lnif -s "$WHMCONFIG/zshrc" "$HOME/.zshrc" 
+else
+  echo "[-] zsh config is not linked."
+fi
+
+lnif -s "$WHMCONFIG/p10k.zsh" "$HOME/.p10k.zsh"
+
+if ensure vim; then
+  lnif -s "$WHMCONFIG/vimrc" "$HOME/.vimrc"
+else
+  echo "[-] vim config is not linked."
+fi
+
+if ensure-all nvim rg; then
+  mkdir -p "$HOME/.config";
+  lnif -s "$WHMCONFIG/nvim" "$HOME/.config/nvim";
+else
+  echo "[-] nvim config is not linked."
+fi
+
