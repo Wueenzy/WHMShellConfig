4e996f31d460418b56eb7ac9d589b9798bd362f4
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-10-20 05:18:38 +0000

    Make installtion file setup oh-my-zsh and powerlevel10k

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+set -e  # Exit immediately if any command returns a non-zero status
+
+USER=${USER:-$(id -u -n)}
+# $HOME is defined at the time of login, but it could be unset. If it is unset,
+# a tilde by itself (~) will not be expanded to the current user's home directory.
+# POSIX: https://pubs.opengroup.org/onlinepubs/009696899/basedefs/xbd_chap08.html#tag_08_03
+HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
+# macOS does not have getent, but this works even if $HOME is unset
+HOME="${HOME:-$(eval echo ~$USER)}"
+
+
 WHMCONFIG="$HOME/.whm_shell"
+
 
 # functions
 
@@ -31,17 +44,12 @@ ensure-all ()
   done
 }
 
-safe-run () # run or quit
-{
-  local program=$1 shift
-  if ! ensure $program;  then
-    exit 1
-  fi
-  $program $@
-}
+
+ensure-all git wget || exit 1;
+
 
 if ! [[ -d "$WHMCONFIG" ]]; then
-  safe-run git clone "https://github.com/VKWHM/WHMShellConfig.git" "$WHMCONFIG"
+  git clone "https://github.com/VKWHM/WHMShellConfig.git" "$WHMCONFIG"
 fi
 
 # Link files
@@ -53,10 +61,20 @@ fi
 if ensure-all zsh fd eza zoxide fzf bat; then
   lnif -s "$WHMCONFIG/myshell" "$HOME/.myshell"
   lnif -s "$WHMCONFIG/zshrc" "$HOME/.zshrc" 
+  if ! [[ -z "$HOME/.oh-my-zsh" ]];then
+    zsh_install_file="$(mktemp)"
+    wget -O $zsh_install_file "https://install.ohmyz.sh/"
+    chmod u+x $zsh_install_file
+    $zsh_install_file --keep-zshrc
+    rm $zsh_install_file
+  fi
 else
   echo "[-] zsh config is not linked."
 fi
 
+if ! [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
 lnif -s "$WHMCONFIG/p10k.zsh" "$HOME/.p10k.zsh"
 
 if ensure vim; then
