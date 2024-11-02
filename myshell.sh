3d94be963e20655c52c5a33c76ac0b4ac746ba55
Harun Koç <82876421+Wueenzy@users.noreply.github.com>
2024-11-02 11:59:33 +0300

    Update myshell.sh

--- myshell.sh
+++ myshell.sh
@@ -59,3 +59,39 @@ function skc() {
     docker run -dtP --rm --expose 22 -p 4445:4445 -p 4444:4444 -p 53:53 $@ --name "$name" --hostname "$name" -v kali-usr:/usr -v kali-var:/var -v kali-etc:/etc -v kali-attacker:/home/attacker kalilinux/kali-rolling /bin/bash && docker exec -u root $name /usr/local/sbin/startup;
     echo "Container '$name' is running. You can connect via ssh $(docker port $name 22 | cut -d ":" -f 2) port.";
 }
+# Kodun bu hali çalışmıyor haberin olsun
+build () {
+  variable = true;
+  for file in $(ls);
+  do 
+    if [ "$file" = "build" ]; then
+      cd "$file";
+      break;
+    fi
+  done
+  if [ $(basename "$(pwd)") = "build" ]; then
+    local file="$(ls)";
+    if [ "${#file[@]}" != "0" ]; then
+      for file in $(ls);
+      do 
+        rm -rf "$file";
+      done
+    fi
+    cmake ..;
+    make 
+    variable = false;
+  else
+    for file in $(ls);
+    do
+      if [ "$file" = "CMakeList.txt" ]; then
+        mkdir build;
+        build;
+        variable = false;
+        break;
+      fi
+    done
+    if [ "variable" = true ]; then
+      echo "[-] $(pwd) is not has CMakeLists.txt!";
+    fi
+  fi
+}
